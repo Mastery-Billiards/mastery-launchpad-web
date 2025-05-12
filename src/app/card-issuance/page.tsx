bf712b4f636544b36c677ebb6b5f081dd2b8b7f7
DungMastery <dungphung.eng@masterybilliards.vn>
2025-05-12 23:09:54 +0700

    add stepper

--- src/app/card-issuance/page.tsx
+++ src/app/card-issuance/page.tsx
@@ -5,13 +5,17 @@ import {
   TextField,
   Button,
   Typography,
-  Divider,
   Avatar,
+  Stepper,
+  Box,
+  Step,
+  StepLabel,
+  StepContent,
 } from '@mui/material'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
-import { useResponsiveValue } from '@/app/hooks/use-responsive-value'
+// import { useResponsiveValue } from '@/app/hooks/use-responsive-value'
 import Webcam from 'react-webcam'
 
 const videoConstraints = {
@@ -22,15 +26,26 @@ const videoConstraints = {
 
 export default function Page() {
   const router = useRouter()
-  const isMobile = useResponsiveValue({ xs: true, md: false })
+  // const isMobile = useResponsiveValue({ xs: true, md: false })
   const webcamRef = useRef<Webcam>(null)
   const [url, setUrl] = useState<string | null>(null)
+  const [activeStep, setActiveStep] = useState(0)
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1)
+  }
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
+
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current?.getScreenshot()
     if (imageSrc) {
       return setUrl(imageSrc)
     }
   }, [webcamRef])
+
   return (
     <>
       <Stack
@@ -56,146 +71,213 @@ export default function Page() {
         spacing={{ xs: 2, md: 4 }}
         alignItems="center"
         justifyContent="center"
-        // height="calc(100vh - 144px)"
+        bgcolor="#FFFFFF"
+        height="calc(100vh - 80px)"
       >
-        <Stack
-          position="relative"
-          bgcolor="#FFFFFF"
-          boxShadow="0 0 0 1px rgba(0,0,0,.04),0 4px 16px -4px rgba(0,0,0,.16)"
-          borderRadius={isMobile ? 0 : '8px'}
-          width={{ xs: '100%', md: 500 }}
-          spacing={2.5}
-          py={3}
-          divider={<Divider sx={{ borderStyle: 'dashed' }} />}
-        >
-          <Stack px={3} spacing={2.5}>
-            <Typography textAlign="center" fontSize={24}>
-              Thông tin khách
-            </Typography>
-            <Stack direction="row" alignItems="center" spacing={2}>
-              <TextField
-                fullWidth
-                size="small"
-                variant="standard"
-                label="SỐ ĐIỆN THOẠI"
-              />
-              <Button variant="outlined" size="large">
-                OK
-              </Button>
-            </Stack>
-            <Stack
-              border="1px solid #A3A7A9"
-              borderRadius="4px"
-              p={1}
-              spacing={1}
+        <Box sx={{ width: { xs: '100%', md: 500 } }}>
+          <Stepper activeStep={activeStep} orientation="vertical">
+            <Step
+            // active={index === activeStep - 1 || index === activeStep}
             >
-              <Stack
-                direction="row"
-                alignItems="center"
-                justifyContent="space-between"
-              >
-                <Typography>Họ và tên:</Typography>
-                <Typography>XXXXXXXXXXXXXXX</Typography>
-              </Stack>
-              <Stack
-                direction="row"
-                alignItems="center"
-                justifyContent="space-between"
-              >
-                <Typography>Số điện thoại:</Typography>
-                <Typography>XXXXXXXXXXXXXXX</Typography>
-              </Stack>
-              <Stack
-                direction="row"
-                alignItems="center"
-                justifyContent="space-between"
-              >
-                <Typography>Ngày sinh:</Typography>
-                <Typography>XX/XX/XXX</Typography>
-              </Stack>
-            </Stack>
-          </Stack>
-          <Stack px={3} spacing={2.5}>
-            <Typography textAlign="center" fontSize={24}>
-              Thông tin thẻ
-            </Typography>
-            <Stack direction="row" alignItems="center" spacing={2}>
-              <TextField
-                fullWidth
-                size="small"
-                variant="standard"
-                label="QUÉT MÃ BARCODE"
-              />
-              <Button variant="outlined" size="large">
-                OK
-              </Button>
-            </Stack>
-            <Stack
-              border="1px solid #A3A7A9"
-              borderRadius="4px"
-              p={1}
-              spacing={1.5}
+              <StepLabel>Thông tin khách</StepLabel>
+              <StepContent>
+                <Stack spacing={2}>
+                  <Stack direction="row" alignItems="center" spacing={2}>
+                    <TextField
+                      fullWidth
+                      size="small"
+                      variant="standard"
+                      label="SỐ ĐIỆN THOẠI"
+                    />
+                    <Button variant="outlined" size="large">
+                      OK
+                    </Button>
+                  </Stack>
+                  <Stack
+                    border="1px solid #A3A7A9"
+                    borderRadius="4px"
+                    p={1}
+                    spacing={1}
+                  >
+                    <Stack
+                      direction="row"
+                      alignItems="center"
+                      justifyContent="space-between"
+                    >
+                      <Typography>Họ và tên:</Typography>
+                      <Typography>XXXXXXXXXXXXXXX</Typography>
+                    </Stack>
+                    <Stack
+                      direction="row"
+                      alignItems="center"
+                      justifyContent="space-between"
+                    >
+                      <Typography>Số điện thoại:</Typography>
+                      <Typography>XXXXXXXXXXXXXXX</Typography>
+                    </Stack>
+                    <Stack
+                      direction="row"
+                      alignItems="center"
+                      justifyContent="space-between"
+                    >
+                      <Typography>Ngày sinh:</Typography>
+                      <Typography>XX/XX/XXX</Typography>
+                    </Stack>
+                  </Stack>
+                </Stack>
+                <Box sx={{ mb: 2 }}>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Continue
+                  </Button>
+                </Box>
+              </StepContent>
+            </Step>
+            <Step
+            // active={index === activeStep - 1 || index === activeStep}
             >
-              <Stack spacing={1}>
-                <Stack
-                  position="relative"
-                  width="100%"
-                  height={{ xs: 190, md: 220 }}
-                >
-                  <Image src="/platium.png" alt="platium" fill />
+              <StepLabel>Thông tin thẻ</StepLabel>
+              <StepContent>
+                <Stack spacing={2}>
+                  <Stack direction="row" alignItems="center" spacing={2}>
+                    <TextField
+                      fullWidth
+                      size="small"
+                      variant="standard"
+                      label="QUÉT MÃ BARCODE"
+                    />
+                    <Button variant="outlined" size="large">
+                      OK
+                    </Button>
+                  </Stack>
+                  <Stack
+                    border="1px solid #A3A7A9"
+                    borderRadius="4px"
+                    p={1}
+                    spacing={1.5}
+                  >
+                    <Stack spacing={1}>
+                      <Stack
+                        position="relative"
+                        width="100%"
+                        height={{ xs: 190, md: 220 }}
+                      >
+                        <Image src="/platium.png" alt="platium" fill />
+                      </Stack>
+                      <Stack
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="space-between"
+                      >
+                        <Typography>Mã thẻ:</Typography>
+                        <Typography>XXXXXXXXXXXXXXXXX</Typography>
+                      </Stack>
+                      <Stack
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="space-between"
+                      >
+                        <Typography>Hạng thẻ:</Typography>
+                        <Typography>Platium</Typography>
+                      </Stack>
+                      <Stack
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="space-between"
+                      >
+                        <Typography>Trạng thái:</Typography>
+                        <Typography>Active</Typography>
+                      </Stack>
+                    </Stack>
+                  </Stack>
                 </Stack>
-                <Stack
-                  direction="row"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Typography>Mã thẻ:</Typography>
-                  <Typography>XXXXXXXXXXXXXXXXX</Typography>
+
+                <Box sx={{ mb: 2 }}>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Continue
+                  </Button>
+                  <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                    Back
+                  </Button>
+                </Box>
+              </StepContent>
+            </Step>
+            <Step
+            // active={index === activeStep - 1 || index === activeStep}
+            >
+              <StepLabel>Ảnh khách hàng</StepLabel>
+              <StepContent>
+                <Stack>
+                  <Webcam
+                    audio={false}
+                    height="100%"
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    width="100%"
+                    videoConstraints={videoConstraints}
+                    imageSmoothing
+                    mirrored
+                  />
+                  {url && (
+                    <Stack alignItems="center">
+                      <Avatar src={url} sx={{ width: 250, height: 250 }} />
+                    </Stack>
+                  )}
+                  <Button onClick={capture}>Capture photo</Button>
                 </Stack>
-                <Stack
-                  direction="row"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Typography>Hạng thẻ:</Typography>
-                  <Typography>Platium</Typography>
+                <Box sx={{ mb: 2 }}>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Continue
+                  </Button>
+                  <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                    Back
+                  </Button>
+                </Box>
+              </StepContent>
+            </Step>
+            <Step
+            // active={index === activeStep - 1 || index === activeStep}
+            >
+              <StepLabel>OTP</StepLabel>
+              <StepContent>
+                <Stack px={3} direction="row" alignItems="center" spacing={2}>
+                  <TextField
+                    fullWidth
+                    size="small"
+                    variant="standard"
+                    label="OTP"
+                  />
+                  <Button variant="outlined" size="large">
+                    Submit
+                  </Button>
                 </Stack>
-                <Stack
-                  direction="row"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Typography>Trạng thái:</Typography>
-                  <Typography>Active</Typography>
-                </Stack>
-              </Stack>
-            </Stack>
-          </Stack>
-          <Stack px={3} spacing={2.5}>
-            <Webcam
-              audio={false}
-              height="100%"
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              width="100%"
-              videoConstraints={videoConstraints}
-              imageSmoothing
-              mirrored
-            />
-            {url && (
-              <Stack alignItems="center">
-                <Avatar src={url} sx={{ width: 250, height: 250 }} />
-              </Stack>
-            )}
-            <Button onClick={capture}>Capture photo</Button>
-          </Stack>
-          <Stack px={3} direction="row" alignItems="center" spacing={2}>
-            <TextField fullWidth size="small" variant="standard" label="OTP" />
-            <Button variant="outlined" size="large">
-              Submit
-            </Button>
-          </Stack>
-        </Stack>
+                <Box sx={{ mb: 2 }}>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Continue
+                  </Button>
+                  <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                    Back
+                  </Button>
+                </Box>
+              </StepContent>
+            </Step>
+          </Stepper>
+        </Box>
       </Stack>
     </>
   )
