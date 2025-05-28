e5e419d1480bb770fd8f6a1859fbf616f97e54ed
DungMastery <dungphung.eng@masterybilliards.vn>
2025-05-28 20:31:50 +0700

    test update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
-import type { NextConfig } from 'next'
+/** @type {import('next').NextConfig} */
 
 const envVars = {
   HOST: 'http://103.90.226.218:8080',
 }
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   env: envVars,
   reactStrictMode: true,
   serverRuntimeConfig: {
     mySecret: envVars.HOST,
+  },
+  experimental: {
+    serverActions: true,
+    appDir: true,
   },
 }
 
